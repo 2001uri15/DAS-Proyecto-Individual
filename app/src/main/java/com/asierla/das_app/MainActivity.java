4daf993e2b6bb5f7f181c8076be353bd16d11007
2001uri15 <2001uri15@gmail.com>
2025-02-06 10:18:44 +0100

    Página de Preferencias

--- app/src/main/java/com/asierla/das_app/MainActivity.java
+++ app/src/main/java/com/asierla/das_app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.asierla.das_app;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,11 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+        String valor = "es";
+        if (extras != null) {
+            valor= extras.getString("idioma");
+        }
+        // Configurar el idioma antes de cargar la vista (opcional)
+        Locale nuevaloc = new Locale(valor);
+        Locale.setDefault(nuevaloc);
+        Configuration configuration =
+                getBaseContext().getResources().getConfiguration();
+        configuration.setLocale(nuevaloc);
+        configuration.setLayoutDirection(nuevaloc);
+
+        Context context =
+                getBaseContext().createConfigurationContext(configuration);
+        getBaseContext().getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
